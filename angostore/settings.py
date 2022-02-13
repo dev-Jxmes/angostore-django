a35a3466474c71520070dcad58af3371adb82289
dev-jxmes <ariclenealfredo@gmail.com>
2022-02-13 17:19:33 +0100

    store and carts functionality

--- angostore/settings.py
+++ angostore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+#STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
-    'angostore/static',
+    os.path.join(BASE_DIR, "static"), # better solution for static files
 ]
 
 # media files configuration
--- carts/views.py
+++ carts/views.py
@@ -50,7 +50,7 @@ def add_cart(request, product_id):
             existing_variation = item.variations.all()
             ex_var_list.append(list(existing_variation))
             id.append(item.id)
-        print(ex_var_list)
+        #print(ex_var_list)
 
         if product_variation in ex_var_list:
             # increase the cart item quantity
