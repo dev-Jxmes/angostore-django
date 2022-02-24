679fd4abae9594353682f79afdb4858cbc8d6722
dev-jxmes <ariclenealfredo@gmail.com>
2022-02-24 18:38:04 +0100

    product order and payments

--- angostore/settings.py
+++ angostore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR /'static'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), # better solution for static files
 ]
@@ -144,10 +146,11 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ariclenealfredo@gmail.com' 
-EMAIL_PASSWORD = '21199921'
+EMAIL_PASSWORD = 'cezdeabaakocoaie'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  
